6f37e6a3437ca594a6d6f44267d48b756a187e90
Georgeshen <georgeshen@lagou.com>
2020-08-05 10:16:32 +0800

    测试2

--- src/com/git/test/GitDemo.java
+++ src/com/git/test/GitDemo.java
@@ -1,7 +1,14 @@
 package com.git.test;
 
+import java.util.Map;
+
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.printf("1231");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("11","test1");
+        jsonObject.put("22","test2");
+        String toJSONString = jsonObject.toJSONString();
+        Map<String,Object> jsVersionMap  = JSONObject.parseObject(toJSONString);
+        System.out.println(JSONUtil.toJsonStr(jsVersionMap));
     }
 }
