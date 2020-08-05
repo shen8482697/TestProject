2595807b83fcda9f1425766b3cf8155273c55d72
Georgeshen <georgeshen@lagou.com>
2020-08-05 12:10:40 +0800

    测试分支1-修改

--- src/com/git/test/GitDemo.java
+++ src/com/git/test/GitDemo.java
@@ -5,10 +5,14 @@ import java.util.Map;
 public class GitDemo {
     static {
         System.out.println("121321");
+        System.out.println("测试分支1");
     }
     public static void main(String[] args) {
         System.out.println(GitDemo.getTest());
     }
+
+
+
 
     public static int getTest(){
         return 1;
