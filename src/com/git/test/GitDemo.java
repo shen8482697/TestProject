cfdc9b3a3a61e382e9147a92bbc069979ed5e58c
Georgeshen <georgeshen@lagou.com>
2020-08-05 12:06:49 +0800

    修改

--- src/com/git/test/GitDemo.java
+++ src/com/git/test/GitDemo.java
@@ -3,12 +3,14 @@ package com.git.test;
 import java.util.Map;
 
 public class GitDemo {
+    static {
+        System.out.println("121321");
+    }
     public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("11","test1");
-        jsonObject.put("22","test2");
-        String toJSONString = jsonObject.toJSONString();
-        Map<String,Object> jsVersionMap  = JSONObject.parseObject(toJSONString);
-        System.out.println(JSONUtil.toJsonStr(jsVersionMap));
+        System.out.println(GitDemo.getTest());
+    }
+
+    public static int getTest(){
+        return 1;
     }
 }
